4ca76778b0de4296be0ef91e0e0202868c919d4f
Yikai Li <yikaili@stanford.edu>
2025-11-25 11:13:12 -0800

    Enhance plotting and download functionality
    Refactor plotting logic and add download options for figures.

--- main.py
+++ main.py
@@ -70,104 +70,216 @@ with left:
         st.stop()
 
 with right:
+    # Pre-calc data
     data = preprocess(df, column, groupby, top_n)
 
-    kwargs = dict(data_frame=data, x=column, y='Rank', color=groupby, log_x=log_rank_size, log_y=log_rank_size, opacity=0.5, template='ggplot2')
-    fig = px.scatter(**kwargs)
+    # Prepare figure placeholders for later downloads
+    rank_fig = None
+    pdf_fig = None
+    pdf_fig_2nd = None
+    hhi_fig = None
+    alpha_fig = None
+    alpha_fig_2nd = None
+    r_fig = None
+    obs_fig = None
+    obs_fig_2nd = None
+
+    # --- Rank vs Size (with OLS / power law overlays) ---
+    kwargs = dict(
+        data_frame=data,
+        x=column,
+        y='Rank',
+        color=groupby,
+        log_x=log_rank_size,
+        log_y=log_rank_size,
+        opacity=0.5,
+        template='ggplot2'
+    )
+    rank_fig = px.scatter(**kwargs)
 
     data['Rank'] = data['Rank'] - 0.5
+
+    data_2nd = None
     if pl_fit:
         mask = data.groupby(groupby)[column].transform(
-            lambda x: x >= powerlaw.Fit(x, verbose=False).power_law.xmin)
+            lambda x: x >= powerlaw.Fit(x, verbose=False).power_law.xmin
+        )
         data_2nd = data[~mask]
         data = data[mask]
-    kwargs.update(dict(data_frame=data, trendline='ols', trendline_options=dict(log_x=True, log_y=True)))
+
+    kwargs.update(dict(
+        data_frame=data,
+        trendline='ols',
+        trendline_options=dict(log_x=True, log_y=True)
+    ))
     ols_fig = px.scatter(**kwargs)
+
     for trace in ols_fig.data:
         if trace.mode == 'lines':
             trace.line.dash = 'dot'
-            fig.add_trace(trace)
-    if pl_fit:
+            rank_fig.add_trace(trace)
+
+    if pl_fit and data_2nd is not None and not data_2nd.empty:
         kwargs['data_frame'] = data_2nd
         ols_fig_2nd = px.scatter(**kwargs)
         for trace in ols_fig_2nd.data:
             if trace.mode == 'lines':
                 trace.line.dash = 'dot'
-                fig.add_trace(trace)
+                rank_fig.add_trace(trace)
 
-    tabs = st.tabs(['Rank vs. Size', 'PDF', 'HHI'])
-    with tabs[0]:
-        st.plotly_chart(fig)
-        # fig.write_json('tmp/rank_size.json')
-    with tabs[1]:
+    tabs_main = st.tabs(['Rank vs. Size', 'PDF', 'HHI'])
+    with tabs_main[0]:
+        st.plotly_chart(rank_fig)
+
+    # --- PDF tab ---
+    with tabs_main[1]:
         pdf = data.groupby(groupby)[column].apply(get_hist).reset_index()
         pdf = pdf.pivot(index=groupby, columns='level_1', values=column).reset_index().explode(['hist', 'bin_edges'])
         pdf = pdf[pdf['hist'] > 0]
-        pdf_fig = px.scatter(pdf, x='bin_edges', y='hist', color=groupby, log_x=True, log_y=True, trendline='ols',
-                             trendline_options=dict(log_x=True, log_y=True),
-                             labels={'hist': 'Probability', 'bin_edges': column})
+        pdf_fig = px.scatter(
+            pdf,
+            x='bin_edges',
+            y='hist',
+            color=groupby,
+            log_x=True,
+            log_y=True,
+            trendline='ols',
+            trendline_options=dict(log_x=True, log_y=True),
+            labels={'hist': 'Probability', 'bin_edges': column}
+        )
         for trace in pdf_fig.data:
             if trace.mode == 'lines':
                 trace.line.dash = 'dot'
         st.plotly_chart(pdf_fig)
-        # pdf_fig.write_json('tmp/pdf.json')
         st.caption('Note: 50 bins for each group')
-        if pl_fit:
+
+        if pl_fit and data_2nd is not None and not data_2nd.empty:
             pdf_2nd = data_2nd.groupby(groupby)[column].apply(get_hist).reset_index()
             pdf_2nd = pdf_2nd.pivot(index=groupby, columns='level_1', values=column).reset_index().explode(
                 ['hist', 'bin_edges'])
             pdf_2nd = pdf_2nd[pdf_2nd['hist'] > 0]
-            pdf_fig_2nd = px.scatter(pdf_2nd, x='bin_edges', y='hist', color=groupby, log_x=True, log_y=True,
-                                     trendline='ols', trendline_options=dict(log_x=True, log_y=True),
-                                     labels={'hist': 'Probability', 'bin_edges': column})
+            pdf_fig_2nd = px.scatter(
+                pdf_2nd,
+                x='bin_edges',
+                y='hist',
+                color=groupby,
+                log_x=True,
+                log_y=True,
+                trendline='ols',
+                trendline_options=dict(log_x=True, log_y=True),
+                labels={'hist': 'Probability', 'bin_edges': column}
+            )
             for trace in pdf_fig_2nd.data:
                 if trace.mode == 'lines':
                     trace.line.dash = 'dot'
             st.plotly_chart(pdf_fig_2nd)
             st.caption('Note: 2nd fit is only for the data above the xmin of the 1st fit')
-    with tabs[2]:
+
+    # --- HHI tab ---
+    with tabs_main[2]:
         hhi = data.groupby(groupby)[column].apply(lambda x: ((x / x.sum())**2).sum() * 10000).reset_index()
         hhi_fig = px.scatter(hhi, x=groupby, y=column)
         st.plotly_chart(hhi_fig)
-        # hhi.to_csv('tmp/hhi.csv', index=False)
 
+    # --- Alpha, R, Obs calculations ---
     ols_results = px.get_trendline_results(ols_fig)
     if groupby == '':
         ols_results[groupby] = column
+
     alpha_gab = ols_results.set_index(groupby)['px_fit_results'].apply(
-        lambda x: pd.Series([-x.params[1] + 1, x.bse[1] * 1.96, 'Gabaix'])).reset_index()
+        lambda x: pd.Series([-x.params[1] + 1, x.bse[1] * 1.96, 'Gabaix'])
+    ).reset_index()
+
     results = data.groupby(groupby)[column].apply(lambda x: powerlaw.Fit(x, xmin=x.min()))
     r_pl = results.apply(lambda x: x.distribution_compare('power_law', 'lognormal')[0]).reset_index()
-    alpha_pl = results.apply(lambda x: pd.Series([x.power_law.alpha, x.power_law.sigma * 1.96, 'Clauset'])).reset_index()
+    alpha_pl = results.apply(
+        lambda x: pd.Series([x.power_law.alpha, x.power_law.sigma * 1.96, 'Clauset'])
+    ).reset_index()
+
     alpha_df = pd.concat([alpha_gab, alpha_pl]).rename(columns={0: 'alpha', 1: 'error', 2: 'model'})
     alpha_df.to_csv('alpha.csv', index=False)
-    tabs = st.tabs(['Alpha', 'R', 'Obs'])
-    with tabs[0]:
-        fig = px.scatter(alpha_df, x=groupby, y='alpha', error_y='error', color='model')
-        st.plotly_chart(fig, use_container_width=True)
-        if pl_fit:
+
+    tabs_stats = st.tabs(['Alpha', 'R', 'Obs'])
+
+    # Alpha tab
+    with tabs_stats[0]:
+        alpha_fig = px.scatter(alpha_df, x=groupby, y='alpha', error_y='error', color='model')
+        st.plotly_chart(alpha_fig, use_container_width=True)
+        if pl_fit and data_2nd is not None and not data_2nd.empty:
             st.caption('Note: 2nd fit is only for the data above the xmin of the 1st fit')
             ols_results_2nd = px.get_trendline_results(ols_fig_2nd)
             if groupby == '':
                 ols_results_2nd[groupby] = column
             alpha_gab_2nd = ols_results_2nd.set_index(groupby)['px_fit_results'].apply(
-                lambda x: pd.Series([-x.params[1] + 1, x.bse[1] * 1.96, 'Gabaix'])).reset_index()
+                lambda x: pd.Series([-x.params[1] + 1, x.bse[1] * 1.96, 'Gabaix'])
+            ).reset_index()
             results_2nd = data_2nd.groupby(groupby)[column].apply(lambda x: powerlaw.Fit(x, xmin=x.min()))
             alpha_pl_2nd = results_2nd.apply(
-                lambda x: pd.Series([x.power_law.alpha, x.power_law.sigma * 1.96, 'Clauset'])).reset_index()
-            alpha_df_2nd = pd.concat([alpha_gab_2nd, alpha_pl_2nd]).rename(columns={0: 'alpha', 1: 'error', 2: 'model'})
-            fig = px.scatter(alpha_df_2nd, x=groupby, y='alpha', error_y='error', color='model')
-            st.plotly_chart(fig, use_container_width=True)
-    with tabs[1]:
-        fig = px.scatter(r_pl, x=groupby, y=column, labels={column: 'R'})
-        st.plotly_chart(fig, use_container_width=True)
+                lambda x: pd.Series([x.power_law.alpha, x.power_law.sigma * 1.96, 'Clauset'])
+            ).reset_index()
+            alpha_df_2nd = pd.concat([alpha_gab_2nd, alpha_pl_2nd]).rename(
+                columns={0: 'alpha', 1: 'error', 2: 'model'}
+            )
+            alpha_fig_2nd = px.scatter(alpha_df_2nd, x=groupby, y='alpha', error_y='error', color='model')
+            st.plotly_chart(alpha_fig_2nd, use_container_width=True)
+
+    # R tab
+    with tabs_stats[1]:
+        r_fig = px.scatter(r_pl, x=groupby, y=column, labels={column: 'R'})
+        st.plotly_chart(r_fig, use_container_width=True)
         st.caption(
-            'R: Loglikelihood ratio of the two distributions’ fit to the data. If greater than 0, the power_law distribution is preferred. If less than 0, the lognormal distribution is preferred.')
-    with tabs[2]:
-        fig = px.scatter(data.groupby(groupby).size().reset_index(), x=groupby, y=0, labels={'0': 'Number of Obs'})
-        st.plotly_chart(fig, use_container_width=True)
-        if pl_fit:
+            'R: Loglikelihood ratio of the two distributions’ fit to the data. '
+            'If greater than 0, the power_law distribution is preferred. '
+            'If less than 0, the lognormal distribution is preferred.'
+        )
+
+    # Obs tab
+    with tabs_stats[2]:
+        obs_counts = data.groupby(groupby).size().reset_index()
+        obs_fig = px.scatter(
+            obs_counts,
+            x=groupby,
+            y=0,
+            labels={'0': 'Number of Obs'}
+        )
+        st.plotly_chart(obs_fig, use_container_width=True)
+        if pl_fit and data_2nd is not None and not data_2nd.empty:
             st.caption('Note: 2nd fit is only for the data above the xmin of the 1st fit')
-            fig = px.scatter(data_2nd.groupby(groupby).size().reset_index(), x=groupby, y=0,
-                             labels={'0': 'Number of Obs'})
-            st.plotly_chart(fig, use_container_width=True)
+            obs_counts_2nd = data_2nd.groupby(groupby).size().reset_index()
+            obs_fig_2nd = px.scatter(
+                obs_counts_2nd,
+                x=groupby,
+                y=0,
+                labels={'0': 'Number of Obs'}
+            )
+            st.plotly_chart(obs_fig_2nd, use_container_width=True)
+
+    # --- Download figures as JSON ---
+    st.markdown("## Download Figures as JSON")
+
+    def download_plotly_json(fig, label, filename):
+        if fig is not None:
+            st.download_button(
+                label=label,
+                data=fig.to_json(),
+                file_name=filename,
+                mime="application/json"
+            )
+
+    col_dl1, col_dl2, col_dl3 = st.columns(3)
+
+    with col_dl1:
+        download_plotly_json(rank_fig, "Download Rank vs Size", "rank_vs_size.json")
+        download_plotly_json(pdf_fig, "Download PDF", "pdf.json")
+        download_plotly_json(hhi_fig, "Download HHI", "hhi.json")
+
+    with col_dl2:
+        download_plotly_json(alpha_fig, "Download Alpha (1st fit)", "alpha.json")
+        download_plotly_json(r_fig, "Download R (loglikelihood ratio)", "r.json")
+        download_plotly_json(obs_fig, "Download Obs counts (1st fit)", "obs.json")
+
+    with col_dl3:
+        # Only meaningful when pl_fit is True and 2nd-fit figs exist
+        download_plotly_json(pdf_fig_2nd, "Download PDF (2nd fit)", "pdf_2nd.json")
+        download_plotly_json(alpha_fig_2nd, "Download Alpha (2nd fit)", "alpha_2nd.json")
+        download_plotly_json(obs_fig_2nd, "Download Obs counts (2nd fit)", "obs_2nd.json")
